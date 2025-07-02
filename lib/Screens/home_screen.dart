ffae1c490279eb55df1f5238393454b8562fc799
Luis Leiva <luis.leiva@utp.ac.pa>
2025-07-01 19:01:35 -0500

    feat: add HomeScreen and MovieItem widgets for displaying popular movies

---
+++ lib/Screens/home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_cheveronazos/Provider/movie_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:movie_app_cheveronazos/Services/movie_service.dart';
+import 'package:movie_app_cheveronazos/Widgets/movie_item.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final movieProvider = Provider.of<MovieProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Popular Movies')),
+      body: FutureBuilder(
+        future: movieProvider.fetchPopularMovies(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: movieProvider.popularMovies.length,
+              itemBuilder: (context, index) {
+                final movie = movieProvider.popularMovies[index];
+                return MovieItem(movie: movie);
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
